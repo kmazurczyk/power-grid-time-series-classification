83cb635aab95330f904b7cf1df38e6bff63cd2b4
Kai Mazurczyk <kaimazurczyk@Kais-MacBook-Pro.local>
2025-02-21 22:33:25 -0700

    data cleansing, feat eng impedance

--- data/load_data.py
+++ data/load_data.py
@@ -67,7 +67,7 @@ class DataPreprocessor:
         self.one_hot_encoder = OneHotEncoder(sparse_output=False)
 
         #---  FEATURES / COLUMNS ---#
-        self.id_cols = ['source_file','sample_id','scenario_type']
+        self.id_cols = ['source_file','sample_id']
 
         self.status_cols = [
             'control_panel_log1','control_panel_log2','control_panel_log3','control_panel_log4', \
@@ -75,16 +75,21 @@ class DataPreprocessor:
             'snort_log1', 'snort_log2', 'snort_log3', 'snort_log4', \
             'R1_status_flag_for_relays','R2_status_flag_for_relays','R3_status_flag_for_relays','R4_status_flag_for_relays']
 
-        self.target_features = ['is_attack','scenario_class','marker']
+        self.target_features = ['is_attack','scenario_class','marker','scenario_type']
 
-        self.R1_features = [i for i in self.data.columns if 'R1' in i]
-        self.R2_features = [i for i in self.data.columns if 'R2' in i]
-        self.R3_features = [i for i in self.data.columns if 'R3' in i]
-        self.R4_features = [i for i in self.data.columns if 'R4' in i]
-        self.magnitudes  = [i for i in self.data.columns if 'magnitude' in i]
+        self.R1_features = [i for i in self.data.columns if 'R1' in i.lower()]
+        self.R2_features = [i for i in self.data.columns if 'R2' in i.lower()]
+        self.R3_features = [i for i in self.data.columns if 'R3' in i.lower()]
+        self.R4_features = [i for i in self.data.columns if 'R4' in i.lower()]
+        self.magnitudes  = [i for i in self.data.columns if 'magnitude' in i.lower()]
+        self.angles      = [i for i in self.data.columns if 'angle' in i.lower()]
+        self.frequencies = [i for i in self.data.columns if 'freq' in i.lower()]
+        self.impedance   = [i for i in self.data.columns if 'impedance' in i.lower()]
+        self.pos_neg_zero = [i for i in self.data.columns if 'zero' in i.lower()]
+        self.control_panels = [i for i in self.data.columns if 'control' in i.lower()]
+        self.snort       = [i for i in self.data.columns if 'snort' in i.lower()]
+        self.status_flags = [i for i in self.data.columns if 'status_flag' in i.lower()]
 
-        self.cut_features = [i for i in self.data.columns if 'frequency' in i] \
-            + ['sample_id','source_file']
 
     def __str__(self):
         return f"DataPreprocessing Class: Contains dataframe: shape {self.data.shape}, columns {self.columns}, several preprocessing and feature engineering methods. Each method returns self for method chaining. See dir()."
@@ -112,6 +117,15 @@ class DataPreprocessor:
         markers = pd.read_csv(marker_file)
         self.data = self.data.merge(right=markers, how='left', on=key)
         return self
+        
+    def impute_value_ffill(self, cols: list, value = None):
+        """for df or series - returns pd.ffill for either NaN or a value (float) that >= will be nullified and imputed (float)"""
+        if cols is None:
+            cols = self.data.columns
+        if value is not None:
+            imputed_df = self.data[cols].map(lambda x: np.nan if x >= value else x)
+        self.data[cols] = imputed_df.ffill(axis=0)
+        return self
 
     # --- TRANSFORMERS --- #
     def std_scale_transform(self, cols: list, fit=True):
@@ -129,24 +143,19 @@ class DataPreprocessor:
         new_cols = self.one_hot_encoder.get_feature_names_out()
         return new_cols, self.one_hot_encoder.transform(encoded_cols)
 
-    # def label_encoder(self, target, fit=True)
-    #     _transformpd.get_dummies(encoded_cols, drop_first=True)
-    #     bin_df_cols = bin_df.columns.to_list()
-    #     df[bin_df_cols] = bin_df
-    #     df.drop(columns=binary_columns,inplace=True)
-    #     self.data[cols] = scaled_cols.apply(lambda x: np.log(x), axis=1)
-    #     return self
-
-    # TO DO imputation columns for inferance
-    #       refactor preprocessing to map columns to dtypes
-
     # --- FEATURE ENGINEERING --- #   
-    def binarize_multi_class_feature(self, feature_name: str, new_name: str, criteria: list):
+    def binarize_classes(self, feature_name: str, new_feature_name: str, criteria: list):
         """for series: reduces multi class labels to binary. labels 1 for class labels found in list else 0"""
         criteria = [str.lower(i) for i in criteria]
-        self.data[new_name] = self.data[feature_name].apply(lambda x: True if x.lower() in criteria else False)
+        self.data[new_feature_name] = self.data[feature_name].apply(lambda x: True if x.lower() in criteria else False)
         return self
     
+    def binarize_values(self, cols: list, value=np.inf):
+        """for df or series - generates binary 0/1 for >= continuous value, for known np.inf values on impedance"""
+        value_cols = self.data[cols].map(lambda x: 1 if x >= value else 0)
+        self.data[cols] = value_cols
+        return self
+
     def binarize_status_flags(self, cols: list):
         """for df or series - reduces several discrete codes found under _status_flag_for_relays - if 0 then False else True"""
         self.data[cols] = self.data[cols].map(lambda x: False if x==0 else True)
@@ -203,7 +212,7 @@ class DataPreprocessor:
             n_samples = self.data['sample_id'].nunique() // 3
         sample = np.random.choice(self.data['sample_id'].unique(), size=n_samples, replace=replace)
         self.data = self.data.loc[self.data['sample_id'].isin(sample),:]
-        return self
+        return self, sample
 
     def get_dataframe(self) -> pd.DataFrame:
         return self.data
@@ -211,7 +220,7 @@ class DataPreprocessor:
     def preprocess_pipline(self, is_sample = False):
         self.cast_data_types() \
             .apply_event_labels(marker_file, 'marker') \
-            .binarize_multi_class_feature('scenario_class', 'is_attack', ['attack']) \
+            .binarize_classes('scenario_class', 'is_attack', ['attack']) \
             .binarize_status_flags([i for i in dp.columns if 'status_flag_for_relays' in i]) \
             .generate_sample_ids() \
             .generate_time_series()
@@ -235,6 +244,7 @@ if __name__ == '__main__':
     df.to_csv(combined_csv)
     print(f"Data written to: {combined_csv}")
 
-    df = dp.sample_data().get_dataframe()
+    dp.sample_data()
+    df = dp.get_dataframe()
     df.to_csv(combined_sample_csv)
     print(f"Data written to: {combined_sample_csv}")
